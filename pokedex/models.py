42883eaf9cf550a5784e7ab332df0ba3430f5678
Danny-1504 <danny.caraguay@uisek.edu.ec>
2025-10-21 10:39:53 -0500

    Laboratorio 4 Danny Caraguay

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    type = models.CharField(max_length=40, null=False)
+    weigth = models.IntegerField(null=False)
+    Heigth= models.IntegerField(null=False)
+    
+    def __str__(self):
+        return self.name
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    ##Trainer 
+    #name:
+    #age:
+    #level:
