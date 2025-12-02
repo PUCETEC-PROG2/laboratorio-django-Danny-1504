814bb31561604c3f3a60765c99970a73d88f413e
Danny-1504 <danny.caraguay@uisek.edu.ec>
2025-12-02 10:40:48 -0500

    Laboratorio 7. API REST con Django Laboratorio 8. Implementación de OAuth 2.0 en Django

--- api/views.py
+++ api/views.py
@@ -2,9 +2,19 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from pokedex.models import Pokemon
 from .serializers import PokemonSerializer
+from oauth2_provider.contrib.rest_framework import TokenHasScope, OAuth2Authentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class PokemonViewSet(viewsets.ModelViewSet):
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
     required_scopes = ['write']
+    
+    authentication_classes = [OAuth2Authentication]
+    required_scopes = ["write"]
+    
+    def get_permissions(self):
+        if self.request.method in ["POST","PUT","PATCH","DELETE"]:
+            return [TokenHasScope(), IsAuthenticated()]
+        return [AllowAny()]
